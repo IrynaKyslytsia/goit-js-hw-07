6f973a57451a1df0ffc8992f4a51c3e74479f458
IrynaKyslytsia <i.kislica89@gmail.com>
2023-03-04 14:35:20 +0200

    02-SimpleLightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" 
+    href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+    </a>`})
+    .join("");
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = galleryMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', 
+{ 
+    caption: true, 
+    captionSelector: 'img[alt]', 
+    captionType: 'attr', 
+    captionsData: 'alt', 
+    captionPosition: 'bottom', 
+    captionDelay: 250 
+}); 
+
 console.log(galleryItems);
+
